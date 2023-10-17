1fbe61709e198d3cef10ab3af8d3f051bb3e05d7
clarnx <clarenceonline@outlook.com>
2023-10-17 21:50:28 +0000

    minor ui changes to search bar

--- src/components/UI/SearchBar.tsx
+++ src/components/UI/SearchBar.tsx
@@ -5,7 +5,7 @@ const SearchBar = () => {
   return (
     <div className="w-100 d-flex justify-content-center align-items-center mt-5">
       <Form.Control
-        className="w-50 shadow-none border border-2 border-secondary-subtle"
+        className="w-50 shadow-none border border-2 border-secondary-subtle opacity-50"
         type="text"
         placeholder="Search tasks"
       />
