bcc6934fe15b40f107fc1090037b483ea43e416c
clarnx <clarenceonline@outlook.com>
2023-10-17 21:44:57 +0000

    styled task search bar

--- src/components/UI/SearchBar.tsx
+++ src/components/UI/SearchBar.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Form } from 'react-bootstrap';
 
 const SearchBar = () => {
   return (
-    <div>SearchBar</div>
-  )
+    <div className="w-100 d-flex justify-content-center align-items-center mt-5">
+      <Form.Control
+        className="w-50 shadow-none border border-2 border-secondary-subtle"
+        type="text"
+        placeholder="Search tasks"
+      />
+    </div>
+  );
 }
 
 export default SearchBar
