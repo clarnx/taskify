cb5d721db4abc5d7072c453059adb706995793dd
clarnx <clarenceonline@outlook.com>
2023-10-17 17:21:38 +0000

    moved floading action button from Layout component to Home component

--- src/components/UI/Layout.tsx
+++ src/components/UI/Layout.tsx
@@ -1,12 +1,10 @@
 import Navbar from "./Navbar";
-import FloatingActionButton from "./AddTaskFloatingActionButton";
 
 const Layout = ({ children }: any) => {
   return (
     <>
       <Navbar />
       <main className="container py-2 px-4 px-md-5 mt-3">{children}</main>
-      <FloatingActionButton/>
     </>
   );
 };
