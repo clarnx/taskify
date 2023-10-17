295365896cdac4eb990246d0665c6b23ff362aa4
clarnx <clarenceonline@outlook.com>
2023-10-17 16:12:59 +0000

    added floating action button in layout

--- src/components/UI/Layout.tsx
+++ src/components/UI/Layout.tsx
@@ -1,11 +1,12 @@
-import React from "react";
 import Navbar from "./Navbar";
+import FloatingActionButton from "./AddTaskFloatingActionButton";
 
 const Layout = ({ children }: any) => {
   return (
     <>
       <Navbar />
       <main className="container py-2 px-4 px-md-5 mt-3">{children}</main>
+      <FloatingActionButton/>
     </>
   );
 };
