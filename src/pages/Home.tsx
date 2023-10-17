d63b38cba47fd1871c732ad206ffc343639950c5
clarnx <clarenceonline@outlook.com>
2023-10-17 17:20:32 +0000

    moved Home component to pages folder

---
+++ src/pages/Home.tsx
@@ -0,0 +1,33 @@
+import AddTaskFloatingActionButton from "../components/UI/AddTaskFloatingActionButton";
+import StatsCard from "../components/Home/StatsCard";
+
+const Home = () => {
+  return (
+    <>
+      <div className="row justify-content-between justify-content-lg-center">
+        <StatsCard
+          taskFilter="All"
+          count={5}
+          iconColor="#235bf5"
+          iconBackgroundColor="#235bf520"
+        />
+        <StatsCard
+          taskFilter="Completed"
+          count={3}
+          iconColor="#6ed146"
+          iconBackgroundColor="#6ed14620"
+        />
+        <StatsCard
+          taskFilter="Uncompleted"
+          count={2}
+          iconColor="#f5a523"
+          iconBackgroundColor="#f5a52320"
+        />
+      </div>
+
+      <AddTaskFloatingActionButton />
+    </>
+  );
+};
+
+export default Home;
