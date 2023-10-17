31c8d9ae23ae3387f4095310ea0390d953f66b66
clarnx <clarenceonline@outlook.com>
2023-10-17 17:14:07 +0000

    add floating action button and add task modal markup

--- src/components/UI/AddTaskFloatingActionButton.tsx
+++ src/components/UI/AddTaskFloatingActionButton.tsx
@@ -1,38 +1,52 @@
 import { useEffect, useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 import { Fab } from "react-tiny-fab";
 
 import "react-tiny-fab/dist/styles.css";
 
 const AddTaskFloatingActionButton = () => {
+  const [showModal, setShowModal] = useState(false);
 
-  const [showFAB, setShowFAB] = useState(false);
+  return (
+    <>
+      <Modal
+        show={showModal}
+        onHide={() => setShowModal(false)}
+        backdrop="static"
+        centered
+      >
+        <Modal.Header className="border-0" closeButton>
+          <Modal.Title>Add Task</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
 
-  const [showModal, setShowModal] = useState(false)
+        <Modal.Footer className="border-0">
+          <Button variant="secondary" onClick={() => setShowModal(false)}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={() => setShowModal(false)}>
+            Add
+          </Button>
+        </Modal.Footer>
+      </Modal>
 
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      setShowFAB(true);
-    }
-  }, []);
-
-  return showFAB === true ? (
-    <Fab
-      mainButtonStyles={{
-        backgroundColor: "#7e56da",
-        borderRadius: "50%",
-      }}
-      style={{
-        bottom: 24,
-        right:  12,
-        zIndex: 1,
-      }}
-      event="click"
-      icon={<i className="bi bi-plus display-6"></i>}
-      alwaysShowTitle={true}
-    //   onClick={}
-    />
-  ) : null;
+      <Fab
+        mainButtonStyles={{
+          backgroundColor: "#7e56da",
+          borderRadius: "50%",
+        }}
+        style={{
+          bottom: 24,
+          right: 12,
+          zIndex: 1,
+        }}
+        event="click"
+        icon={<i className="bi bi-plus display-6"></i>}
+        alwaysShowTitle={true}
+        onClick={() => setShowModal(true)}
+      />
+    </>
+  );
 };
 
 export default AddTaskFloatingActionButton;
