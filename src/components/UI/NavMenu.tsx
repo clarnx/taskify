2758bb46d94c08ef9cd55b73be5814b43c49c87b
clarnx <clarenceonline@outlook.com>
2023-10-18 16:43:35 +0000

    added nav menu item active state

--- src/components/UI/NavMenu.tsx
+++ src/components/UI/NavMenu.tsx
@@ -1,18 +1,24 @@
 const NavMenu = () => {
+  const { pathname } = window.location;
+
   return (
     <div className="">
       <ul className="navbar-nav flex-row justify-content-center align-items-center flex-grow-1 fs-md-6 gap-3">
         <li className="nav-item">
           <a
             href="/"
-            // className={`nav-link ${pathname === "/" ? "active fw-bold" : ""}`}
-            className={`nav-link "active fw-bold"}`}
+            className={`nav-link ${pathname === "/" ? "active fw-bold" : ""}}`}
           >
             Home
           </a>
         </li>
         <li className="nav-item">
-          <a href="/task-category" className={`nav-link "active fw-bold"}`}>
+          <a
+            href="/task-category"
+            className={`nav-link ${
+              pathname === "/task-category" ? "active fw-bold" : ""
+            }}`}
+          >
             Category
           </a>
         </li>
