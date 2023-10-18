b98a95275e9a6561aebfd8ea6a67b080fb6e74b8
clarnx <clarenceonline@outlook.com>
2023-10-18 16:27:50 +0000

    added delete task, mark task complete, and search task methods to store

--- src/hooks/appDataStore.ts
+++ src/hooks/appDataStore.ts
@@ -40,7 +40,73 @@ const useAppDataStore = create((set: any) => ({
         };
       }
     ),
+  setTaskSearchResults: (query: string) =>
+    set(
+      (state: {
+        tasks: string[];
+        currentTask: any;
+        taskCategoryList: string[];
+        taskSearchResults: string[];
+      }) => {
+        const appDataInLocalStorage = localStorage.getItem("appData") as string;
 
+        const parsedAppDataInLocalStorage = JSON.parse(appDataInLocalStorage);
+
+        // Logic to set taskSearchResults based on taskName or description or priority or category or status.
+        // Search is partial search
+
+        const taskSearchResults = parsedAppDataInLocalStorage?.tasks?.filter(
+          (task: any) => {
+            return Object.values(task)?.some(
+              (val) =>
+                typeof val === "string" &&
+                val.toLowerCase().includes(query.toLowerCase())
+            );
+          }
+        );
+
+        return {
+          ...state,
+          taskSearchResults,
+        };
+      }
+    ),
+  markTaskAsComplete: (currentTaskId: string) =>
+    set(
+      (state: {
+        tasks: string[];
+        currentTask: any;
+        taskCategoryList: string[];
+        taskSearchResults: string[];
+      }) => {
+        const appDataInLocalStorage = localStorage.getItem("appData") as string;
+
+        const parsedAppDataInLocalStorage = JSON.parse(appDataInLocalStorage);
+
+        parsedAppDataInLocalStorage.tasks =
+          parsedAppDataInLocalStorage?.tasks?.map((task: any) => {
+            const updatedStatus =
+              task?.status === "completed" ? "in-progress" : "completed";
+            if (task?.id === currentTaskId) {
+              return { ...task, status: updatedStatus };
+            } else {
+              return task;
+            }
+          });
+
+        // Add task to local storage
+        localStorage.setItem(
+          "appData",
+          JSON.stringify(parsedAppDataInLocalStorage)
+        );
+
+        return {
+          ...state,
+          tasks: [...parsedAppDataInLocalStorage.tasks],
+          currentTask: initalTaskDetails,
+        };
+      }
+    ),
   addTask: (currentTask: any) =>
     set(
       (state: {
@@ -57,6 +123,38 @@ const useAppDataStore = create((set: any) => ({
           ...parsedAppDataInLocalStorage.tasks,
           currentTask,
         ];
+
+        // Add task to local storage
+        localStorage.setItem(
+          "appData",
+          JSON.stringify(parsedAppDataInLocalStorage)
+        );
+
+        return {
+          ...state,
+          tasks: [...parsedAppDataInLocalStorage.tasks],
+          currentTask: initalTaskDetails,
+        };
+      }
+    ),
+
+  deleteTask: (currentTaskId: string) =>
+    set(
+      (state: {
+        tasks: string[];
+        currentTask: any;
+        taskCategoryList: string[];
+        taskSearchResults: string[];
+      }) => {
+        const appDataInLocalStorage = localStorage.getItem("appData") as string;
+
+        const parsedAppDataInLocalStorage = JSON.parse(appDataInLocalStorage);
+
+        const updatedTaskList = parsedAppDataInLocalStorage?.tasks?.filter(
+          (task: any) => task?.id !== currentTaskId
+        );
+
+        parsedAppDataInLocalStorage.tasks = [...updatedTaskList];
 
         // Add task to local storage
         localStorage.setItem(
