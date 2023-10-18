16b4036592a2bb47a4fee038c5e692fab56b526c
clarnx <clarenceonline@outlook.com>
2023-10-18 06:18:57 +0000

    added task category markup

---
+++ src/pages/TaskCategory.tsx
@@ -0,0 +1,11 @@
+const TaskCategory = () => {
+  return (
+    <div className="row justify-content-between justify-content-lg-center m-0">
+      <span className="w-100 w-md-50 fw-bolder">
+        <h4 className="mb-4">Task Categories</h4>
+      </span>
+    </div>
+  );
+};
+
+export default TaskCategory;
