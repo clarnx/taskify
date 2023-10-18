0e25492d5babdfeab875be68423e8957e51224c2
clarnx <clarenceonline@outlook.com>
2023-10-18 06:16:53 +0000

    updated app data state with extra task data

--- src/hooks/appDataStore.ts
+++ src/hooks/appDataStore.ts
@@ -2,17 +2,29 @@ import { create } from "zustand";
 
 const useAppDataStore = create((set: any) => ({
   tasks: [],
-  taskCategory: [],
+  currentTask: {},
+  taskCategoryList: [],
+  taskSearchResults: [],
   initAppDataStore: () =>
-    set((state: { tasks: string[]; taskCategory: string[] }) => {
-      const appDataInLocalStorage = localStorage.getItem("appData") as string;
+    set(
+      (state: {
+        tasks: string[];
+        currentTask: any;
+        taskCategoryList: string[];
+        taskSearchResults: string[];
+      }) => {
+        const appDataInLocalStorage = localStorage.getItem("appData") as string;
 
-      const parsedAppDataInLocalStorage = JSON.parse(appDataInLocalStorage);
-      return {
-        tasks: parsedAppDataInLocalStorage.tasks,
-        taskCategory: parsedAppDataInLocalStorage.taskCategory,
-      };
-    }),
+        const parsedAppDataInLocalStorage = JSON.parse(appDataInLocalStorage);
+
+        return {
+          tasks: parsedAppDataInLocalStorage.tasks,
+          currentTask: state.currentTask,
+          taskCategoryList: parsedAppDataInLocalStorage.taskCategory,
+          taskSearchResults: state.taskCategoryList,
+        };
+      }
+    ),
 }));
 
 export default useAppDataStore;
