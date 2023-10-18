8858419186700560a76423a0b5e0a58177afa35d
clarnx <clarenceonline@outlook.com>
2023-10-18 16:28:48 +0000

    formatted search bar component with markup fixes

--- src/components/UI/SearchBar.tsx
+++ src/components/UI/SearchBar.tsx
@@ -1,15 +1,22 @@
-import { Form } from 'react-bootstrap';
+import { Form } from "react-bootstrap";
 
-const SearchBar = ({placeholder = "Search"}: {placeholder: string}) => {
+const SearchBar = ({
+  placeholder = "Search",
+  onInputHandler = () => {},
+}: {
+  placeholder: string;
+  onInputHandler: any;
+}) => {
   return (
     <div className="w-100 d-flex justify-content-center align-items-center mt-5">
       <Form.Control
         className="w-100 w-md-50 shadow-none border border-2 border-secondary-subtle opacity-50"
         type="text"
         placeholder={placeholder}
+        onInput={onInputHandler}
       />
     </div>
   );
-}
+};
 
-export default SearchBar
+export default SearchBar;
