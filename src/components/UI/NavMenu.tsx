2d5c696957055bbb22b2f1309af7fa709f9af0dd
clarnx <clarenceonline@outlook.com>
2023-10-18 21:26:48 +0000

    minor ui changes

--- src/components/UI/NavMenu.tsx
+++ src/components/UI/NavMenu.tsx
@@ -4,21 +4,17 @@ const NavMenu = () => {
   return (
     <div className="">
       <ul className="navbar-nav flex-row justify-content-center align-items-center flex-grow-1 fs-md-6 gap-3">
-        <li className="nav-item">
-          <a
-            href="/"
-            className={`nav-link ${pathname === "/" ? "active fw-bold" : ""}}`}
-          >
+        <li className={`nav-item ${pathname === "/" ? "active fw-bold" : ""}`}>
+          <a href="/" className="nav-link">
             Home
           </a>
         </li>
-        <li className="nav-item">
-          <a
-            href="/task-category"
-            className={`nav-link ${
-              pathname === "/task-category" ? "active fw-bold" : ""
-            }}`}
-          >
+        <li
+          className={`nav-item ${
+            pathname === "/task-category" ? "active fw-bold" : ""
+          }`}
+        >
+          <a href="/task-category" className="nav-link">
             Category
           </a>
         </li>
